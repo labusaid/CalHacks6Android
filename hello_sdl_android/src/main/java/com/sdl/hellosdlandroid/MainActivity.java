504d009096605aaac0a7911564beaf462f82b490
labusaid <labusaid@gmail.com>
2019-10-26 19:59:18 -0700

    safety commit

--- hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
+++ hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
@@ -3,28 +3,69 @@ package com.sdl.hellosdlandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "MainActivity";
+
+	final FirebaseDatabase database = FirebaseDatabase.getInstance();
+	DatabaseReference ref = database.getReference();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//If we are connected to a module we want to start our SdlService
-		if(BuildConfig.TRANSPORT.equals("MULTI") || BuildConfig.TRANSPORT.equals("MULTI_HB")) {
-			SdlReceiver.queryForConnectedService(this);
-		}else if(BuildConfig.TRANSPORT.equals("TCP")) {
-			Intent proxyIntent = new Intent(this, SdlService.class);
-			startService(proxyIntent);
+		SdlReceiver.queryForConnectedService(this);
+
+		//button
+		Button submitButton = findViewById(R.id.submitButton);
+		submitButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			// Write a message to the database
+				Log.d(TAG, "test1");
+				newEntry("test4",1,2,3);
+			}
+		});
+	}
+
+	@IgnoreExtraProperties
+	public class Driver {
+
+		public double currentSpeed;
+		public int distanceDriven;
+		public int timeMoving;
+
+		public Driver() {
+			// Default constructor required for calls to DataSnapshot.getValue(User.class)
+			Log.d(TAG, "test2");
 		}
+
+		public Driver(double speed, int distanceDriven, int timeMoving) {
+			this.currentSpeed = speed;
+			this.distanceDriven = distanceDriven;
+			this.timeMoving = timeMoving;
+		}
+
+	}
+
+	private void newEntry(String name, double currentSpeed, int distanceDriven, int timeMoving) {
+		Log.d(TAG, "test3");
+
+		Driver driver = new Driver(currentSpeed, distanceDriven, timeMoving);
+
+		ref.child(name).setValue(driver);
 	}
 
 	@Override
@@ -46,11 +87,4 @@ public class MainActivity extends AppCompatActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/** Called when the user clicks submit */
-	FirebaseDatabase database = FirebaseDatabase.getInstance();
-	public void sendName () {
-		// Write a message to the database
-		DatabaseReference ref = database.getReference("drivers");
-		ref.setValue("test");
-	}
 }
